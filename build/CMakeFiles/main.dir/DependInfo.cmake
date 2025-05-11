8fc03579a42571c18b776ca676757da0ca5075cd
ofeiipe <costadecarvalho.felipe@gmail.com>
2025-05-11 16:47:28 -0300

    adding cmake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Projetos/ProjetoCSFML/interfaces/pauseMenu.c" "CMakeFiles/main.dir/interfaces/pauseMenu.c.obj" "gcc" "CMakeFiles/main.dir/interfaces/pauseMenu.c.obj.d"
+  "C:/Projetos/ProjetoCSFML/main.c" "CMakeFiles/main.dir/main.c.obj" "gcc" "CMakeFiles/main.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
